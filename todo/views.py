70030e07fddadb7fed0e0ad0a0e4e669b33d63e8
Michal Hozza <mhozza@gmail.com>
2016-03-31 20:11:58 +0200

    removed wrong docstring

--- todo/views.py
+++ todo/views.py
@@ -5,8 +5,5 @@ from .serializers import ToDoSerializer
 
 
 class ToDoViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
